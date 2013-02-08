de4d92fede4179d2fcf89a9960e351a81a7c546a
Admin <pasha@ubuntu.ubuntu-domain>
2013-02-08 10:05:19 +0700

    Initila commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def jobs
+  end
+
+  def monthly
+  end
+
+  def privacy
+  end
+
+  def airport
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def daily
+  end
+
+  def dailysearch
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StagePark::Application.config.secret_token = '92818be0e50479ff2b28b18d7303ce00c00afeb14376b73a96ef70246c001b28b7d04e9dee81a8ab99d08a1b0f1d2f566b0498850e3a43ccab61ac528ae52aa7'
